fddeefca076be0576f17c71a6eae106d1c772772
mkatara <manishkatara@gmail.com>
2018-09-08 19:16:39 +0530

    List Close feature
    also fixed following-
    1. list id generation and persistence
    2. URL configuration using url_for and app.config[]
    3. redirect method

--- grocery.py
+++ grocery.py
@@ -1,8 +1,9 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from db import dbops
 
 app = Flask(__name__)
+app.config['SERVER_NAME']='127.0.0.1:5000'
 dbc = dbops()
 #vegetables = ['potato','tomato','onions','okara]
 vegetableRow = list()
@@ -10,36 +11,40 @@ vegetableSet = list()
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    dbc.globalistid = dbc.gen_list_id()#to set a global listid value
     return render_template('dashboard.html')
 
 @app.route('/CreateList.html',methods=['GET','POST'])
 def createList():
-    global vegetables
     if request.method == 'POST':
-        x=request.form['item']
+        itemname=request.form['item']
         QUANTITY=request.form['QUANTITY']
         BASKETED=request.form['BASKETED']
         price=request.form['price']
         PURCHASEDFROM=request.form['PURCHASEDFROM']
-        vegetableRow.append(x)
+        vegetableRow=[]
+        vegetableRow.append(itemname)
         vegetableRow.append(QUANTITY)
         vegetableRow.append(BASKETED)
         vegetableRow.append(price)
         vegetableRow.append(PURCHASEDFROM)
-
+        listid=dbc.globalistid
         vegetableSet.append(vegetableRow)
-        veg=dict(NAME=x,QUANTITY='QUANTITY',PURCHASED=BASKETED,PRICE=price,PURCHASEDFROM=PURCHASEDFROM,ISLISTOPEN='Y')
-        dbc.ins_row(veg)
+        veg=dict(NAME=itemname,QUANTITY=QUANTITY,PURCHASED=BASKETED,PRICE=price,PURCHASEDFROM=PURCHASEDFROM,ISLISTOPEN='Y')
+        dbc.ins_row(veg,listid)
+
     return render_template('CreateList.html',listitems=vegetableSet)
 
 @app.route('/showReport.html', methods=['GET','POST'])
 def showReport():
-    if request.method=='GET':
-        #call db to get rows
+    #if request.method=='GET':
         purchases=dbc.fetch_row()
-        #purchases=dict([('grapes','29-08-2018'),('apples','29-08-2018')])
-        #for r in purchases:
         return render_template('showReport.html',itemset=purchases)
+
+@app.route('/closeList/<lstid>')
+def closeList(lstid):
+    dbc.closeList(lstid)
+    return redirect(url_for('showReport'))
 
 
 if __name__=='__main__':
